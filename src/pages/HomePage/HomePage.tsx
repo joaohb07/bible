91834d502f145e30f7622308de1d7445c577c955
joaohb07 <joaoh072002@gmail.com>
2026-01-12 16:29:45 +0000

    fix(pages): Change open book text

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <div style={{ padding: 24 }}>
       <h1>The Bible</h1>
-      <a href={`${import.meta.env.BASE_URL}read/pt/genesis/1`}>Começar</a>
+      <a href={`${import.meta.env.BASE_URL}read/pt/genesis/1`}>Open</a>
     </div>
   );
 }
